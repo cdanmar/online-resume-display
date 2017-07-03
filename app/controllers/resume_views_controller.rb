d1d2bcc3e2c0430e5952ed00147a7c4f4e5cdd39
victorclee <victorchristopherlee@gmail.com>
2017-07-03 18:57:09 -0500

    hardcoded resume info

--- app/controllers/resume_views_controller.rb
+++ app/controllers/resume_views_controller.rb
@@ -6,7 +6,40 @@ class ResumeViewsController < ApplicationController
 
   def show
     # @resume = Unirest.get("").body
-    @resume = 
-  end
+    @resume =   {
+                  "first_name": "Bobby",
+                  "last_name": "Boo",
+                  "email": "bobby.boo@gmail.com",
+                  "phone_number": "312-334-5567",
+                  "short_bio": "great guy",
+                  "linkedin_url": "www.linkedin.com/bobbyboo",
+                  "twitter_handle": "bobbyboo",
+                  "personal_website": "www.bobbyboo.com",
+                  "online_resume_url": "www.resume.com/bobbyboo",
+                  "github_url": "www.github.com/bobbyboo",
+                  "photo": "nil",
+                  "experience": {
+                                  "start_date": "2016-05-20",
+                                  "end_date": "currently employed here",
+                                  "job_title": "web developer",
+                                  "company_name": "braintree",
+                                  "details": "oh what a dream job"
+                                  },
+                  "education": {
+                                  "start_date": "2012-08-15",
+                                  "end_date": "2016-05-19",
+                                  "degree": "information technology",
+                                  "university_name": "Actualize",
+                                  "details": "time of his life"
+                                  },
+                  "skills": "Ruby on Rails, JavaScript",
+                  "capstone": {
+                                  "name": "cheeseit",
+                                  "description": "just as it sounds",
+                                  "url": "www.heroku.com/cheeseit",
+                                  "screenshot": "nil"
+                                  }
+                }
 
+  end
 end
---
+++ sample_student.rb
@@ -0,0 +1,34 @@
+  {
+    "first_name": "",
+    "last_name": "",
+    "email": "",
+    "phone_number": "",
+    "short_bio": "",
+    "linkedin_url": "",
+    "twitter_handle": "",
+    "personal_website": "",
+    "online_resume_url": "",
+    "github_url": "",
+    "photo": "",
+    "experience": {
+                    "start_date": "",
+                    "end_date": "",
+                    "job_title": "",
+                    "company_name": "",
+                    "details": ""
+                    },
+    "education": {
+                    "start_date": "",
+                    "end_date": "",
+                    "degree": "",
+                    "university_name": "",
+                    "details": ""
+                    },
+    "skills": "",
+    "capstone": {
+                    "name": "",
+                    "description": "",
+                    "url": "",
+                    "screenshot": ""
+                    }
+  }
